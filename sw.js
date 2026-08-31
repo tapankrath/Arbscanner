a4ea59148b91328d0f41d56e67017b2202a8d1ab
tapankrath <tapanr@gmail.com>
2026-08-31 11:48:01 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE = 'spread-v1';
+const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  // Network-first for the live API calls (kalshi/polymarket), cache-first for our own shell files.
+  const url = e.request.url;
+  const isOwnAsset = ASSETS.some((a) => url.endsWith(a.replace('./', '')));
+  if (isOwnAsset) {
+    e.respondWith(caches.match(e.request).then((cached) => cached || fetch(e.request)));
+  }
+});
