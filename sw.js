3a8f6187b980b8be457878ec3c452a6e0a17bbcd
tapankrath <tapanr@gmail.com>
2026-08-31 18:36:18 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'spread-v18';
+const CACHE = 'spread-v19';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
